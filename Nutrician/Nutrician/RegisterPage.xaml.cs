df2925597cc3c9036f55657304d09ebe488a3f01
swara <swara@DESKTOP-QNAG2RA>
2022-10-17 13:28:37 -0700

    Add project files.

---
+++ Nutrician/Nutrician/RegisterPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nutrician
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnGenderRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            // Perform required operation
+        }
+
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HomePage());
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
