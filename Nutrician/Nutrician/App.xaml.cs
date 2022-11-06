2248dc2bfe36589568a457430322db047b406379
Swara Ramesh <swara.ramesh@outlook.com>
2022-11-05 22:02:21 -0700

    11/5 10:02pm

--- Nutrician/Nutrician/App.xaml.cs
+++ Nutrician/Nutrician/App.xaml.cs
@@ -26,5 +26,19 @@ namespace Nutrician
         protected override void OnResume()
         {
         }
+
+        public void createMedicalConditions()
+        {
+            String[] avoidHeartDisease = { "oil", "salt", "excessive carbs" };
+            String[] avoidKidneyStones = { "salt", "oil", "greens like XXX"};
+            String[] avoidAnemia = { "blood thinners like XXX", "oil"};
+            String[] suggestionsHeartDisease = { "nuts", "fiber", "cardio"};
+            String[] suggestionsKidneyStones = { "nuts", "beans", "fiber" };
+            String[] suggestionsAnemia = { "reds like pomegranate", "beet root", "water"};
+            String[] meals = { "Pasta", "Pizza"};
+            String[] veganMeals = { "Vegan Pasta", "Rotis"};
+            String[] lactoseIntolerantMeals = { "MNM", "milk-less chapati"};
+            MedicalCondition cond1 = new MedicalCondition("Heart Disease", suggestionsHeartDisease, avoidHeartDisease, meals, veganMeals, lactoseIntolerantMeals);
+        }
     }
 }
