0003937bb5d43db0c756423189cd1961e4d3fd4f
Swara Ramesh <swararamesh@Swaras-MacBook-Air.local>
2023-01-02 21:46:15 -0800

    jan 2 9:46pm

---
+++ Nutrician/Nutrician/Helpers/TrackerPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Nutrician
+{	
+	public partial class TrackerPage : ContentPage
+	{	
+		public TrackerPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
